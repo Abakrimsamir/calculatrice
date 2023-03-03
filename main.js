b898bbaee56f1ead9f24c2f8cb3deaedcffab3b1
Samir Abakrim <123333480+Abakrimsamir@users.noreply.github.com>
2023-03-03 11:11:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 function Solve(val) {
-    var v = document.getElementById('res')
-    v.value += val;
+    
+    var v = document.getElementById('res') 
+    v.innerHTML += val;
 }
 function Result() {
-    var num2 = eval(document.getElementById('res').value);
-    document.getElementById('res').value = num2;
+    console.log("kkk")
+    var num2 = eval(document.getElementById('res').innerHTML);
+    document.getElementById('res').innerHTML = num2;
 }
 function Clear() {
     var inp = document.getElementById('res');
-    inp.value ='';
+    inp.innerHTML ='';
 }
 function Back() {
     var ev = document.getElementById('res');
-    ev.value = ev.value.slice(0,-1);
+    ev.innerHTML = ev.innerHTML.slice(0,-1);
 }
