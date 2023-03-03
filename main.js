2466571fa5e825f1653f6b5678a4f7900a072c99
Samir Abakrim <123333480+Abakrimsamir@users.noreply.github.com>
2023-03-03 09:36:27 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+let a = 12;
+
+let b = 19;
+let test = [1,2,3]
+
+let list = [
+    {
+        nom : "ahmed",
+        age : 18,
+    } ,
+    {
+        nom : "ali",
+        age : 20,
+    } ,
+    {
+        nom : "moh",
+        age : 14,
+    } 
+]
+console.log(x)
+if(list[1].age > 18 ){
+    console.log("allowed");
+} 
+else{
+    console.log("not allowed");
+}
